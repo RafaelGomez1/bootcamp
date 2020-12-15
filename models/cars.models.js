837f147c84252d751549a27ec22fe8172aae8ca8
RafaelGomez1 <rokitoki97@gmail.com>
2020-12-15 20:49:44 +0100

    Divide the schema into N

--- models/cars.models.js
+++ models/cars.models.js
@@ -1,34 +1,15 @@
 const mongoose = require('mongoose');
 
+require Company;
+
 const carSchema = new mongoose.Schema({
-    company: {
-        type: String,
-        required: true,
-        uppercase: true,
-        trim: true,
-        minlength: 2,
-        maxlength: 99,
-        enum: ['BMW', 'AUDI', 'SEAT'] // solo se van a permitir estas compañias
-    },
+    company: Company,
     model: String,
     sold: Boolean,
-    price: {
-        type: Number,
-        required: function() {
-            return this.sold
-        },
-        minlength: 1000
-    },
-    year: {
-        type: Number,
-        min: 2000,
-        max: 2030
-    },
+    price: Price,
+    year: Year,
     extras: [String],
-    created: {
-        type: Date,
-        default: Date.now()
-    }
+    createdAt: Created
 })
 
 const Car = mongoose.model('car', carSchema);
